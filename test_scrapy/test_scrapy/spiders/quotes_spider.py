78dde07cc0aee665171ffb3e776d95fe705e20b1
USER <muhammaddaffa0606@gmail.com>
2023-06-16 10:34:46 +0700

    curry scraping

--- test_scrapy/test_scrapy/spiders/quotes_spider.py
+++ test_scrapy/test_scrapy/spiders/quotes_spider.py
@@ -2,15 +2,15 @@ from pathlib import Path
 
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
     # Override
     def start_requests(self):
         urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
+            # "https://quotes.toscrape.com/page/1/",
+            # "https://quotes.toscrape.com/page/2/",
+            "https://www.basketball-reference.com/players/c/curryst01.html"
         ]
 
         for url in urls:
@@ -18,7 +18,13 @@ class QuotesSpider(scrapy.Spider):
 
     # Override
     def parse(self, res):
-        page = res.url.split('/')[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(res.body)
-        self.log(f"Saved file {filename}")
+        # page = res.url.split('/')[-2]
+        # filename = f"quotes-{page}.html"
+        # Path(filename).write_bytes(res.body)
+        # self.log(f"Saved file {filename}")
+        for quote in res.css("div.quote"):
+            yield {
+                "text" : quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
